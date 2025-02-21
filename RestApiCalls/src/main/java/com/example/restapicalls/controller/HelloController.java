e2daab9ac4b9c8603fd0af5b45da95fed4dcc024
ankit_049 <ankitrajputji00@gmail.com>
2025-02-21 19:46:48 +0530

    [Ankit Rajput0 Add. Created a Get Request method with parameter

--- RestApiCalls/src/main/java/com/example/restapicalls/controller/HelloController.java
+++ RestApiCalls/src/main/java/com/example/restapicalls/controller/HelloController.java
@@ -2,14 +2,21 @@ package com.example.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-
+    // Step-1 Create a method to get the message
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // Step-2 Creating a method to get the request using parameter name
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
